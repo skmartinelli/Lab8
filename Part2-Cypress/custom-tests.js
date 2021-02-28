9a7de13d4e0e4c3f45c25b919e1815982f01367b
skmartinelli <53798189+skmartinelli@users.noreply.github.com>
2021-02-27 23:54:21 -0800

    Add files via upload

--- Part2-Cypress/custom-tests.js
+++ Part2-Cypress/custom-tests.js
@@ -1,9 +1,43 @@
 describe('Party Horn Tests', () => {
   beforeEach(() => {
-    cy.visit('http://127.0.0.1:5500/');
+    cy.visit('http://127.0.0.1:5500/Part2-Cypress');
   });
 
   it('First Test', () => {
     expect(true).to.equal(true);
   });
+
+  it('Slider changes when volume input changes', () => {
+    cy.get('#volume-number').clear().type('75')
+    cy.get('#volume-slider').then($el => {
+      expect($el).to.have.value(75)
+    })
+  })
+
+  it('Slider changes when volume input changes', () => {
+    cy.get('#volume-number').clear().type('75');
+    cy.get('#volume-slider').then($el => {
+      expect($el).to.have.value(75)
+    })
+  })
+
+  it('Volume changes when slider input changes', () => {
+    cy.get('#volume-slider').invoke('val', 33).trigger('input');
+    cy.get('#volume-number').then($el => {
+      expect($el).to.have.value(33)
+    })
+  })
+
+  it('Volume of audio element changes when slider input changes', () => {
+    cy.get('#volume-slider').invoke('val', 33).trigger('input');
+    cy.get('#horn-sound').then($el => {
+      expect($el).to.have.prop('volume', 0.33)
+    }) 
+  })
+
+  
+
+
+
 });
+
